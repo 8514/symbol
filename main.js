3203d13b6bc84a213e0631f727b1ab656059820a
8514 <79288367+8514@users.noreply.github.com>
2021-05-18 17:21:48 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+ctx.beginpath();
+ctx.strokestyle="grey";
+ctx.linewidth= 4;
+ctx.rect(150,143,430,200);
+ctx.stroke();
+
+ctx.beginpath();
+ctx.strokestyle="blue";
+ctx.linewidth= 5;
+ctx.arc(250,210,40,0,2 *Math.PI);
+ctx.stroke();
+
+ctx.beginpath();
+ctx.strokestyle="black";
+ctx.linewidth= 5;
+ctx.arc(350,210,40,0,2 *Math.PI);
+ctx.stroke();
+
+ctx.beginpath();
+ctx.strokestyle="red";
+ctx.linewidth= 5;
+ctx.arc(450,210,40,0,2 *Math.PI);
+ctx.stroke();
+
+ctx.beginpath();
+ctx.strokestyle="yellow";
+ctx.linewidth= 5;
+ctx.arc(300,250,40,0,2 *Math.PI);
+ctx.stroke();
+
+ctx.beginpath();
+ctx.strokestyle="green";
+ctx.linewidth= 5;
+ctx.arc(400,250,40,0,2 *Math.PI);
+ctx.stroke();
